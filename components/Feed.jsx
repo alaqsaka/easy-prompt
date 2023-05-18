357b0dc02dac3d715b483793eda0e9cb0f4284e5
alaqsaka <aqsaabidin15@gmail.com>
2023-05-18 16:48:46 +0700

    fix

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -47,7 +47,7 @@ const Feed = () => {
         />
       </form>
 
-      <PromptCardList data={posts} handleTagClick={() => {}} />
+      {posts && <PromptCardList data={posts} handleTagClick={() => {}} />}
     </section>
   );
 };
